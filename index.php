e77c60627f1dba6fa51f55859e6593fc03817947
Supakorn prabtook <59908327+62310018@users.noreply.github.com>
2020-03-24 10:01:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php echo "รหัสนิสิต-62310018"; ?><br>
 <?php echo "ชื่อ-สกุล นาย ศุภกรณ์ ปราบทุกข์"; ?><br>
 <iframe width="500" height="500"
-src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
+src="https://www.youtube.com/embed/n4im1b2LjTc?autoplay=1">
 </iframe>
 </BODY>
 </HTML>
